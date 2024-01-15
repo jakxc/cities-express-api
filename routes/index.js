2b7d8f28d411ca68752d547fb6b6a6de0de7e610
Jack <jack.x.chen@outlook.com>
2024-01-15 17:15:48 +1000

    Added country data and pagination

--- routes/index.js
+++ routes/index.js
@@ -7,53 +7,100 @@ router.get("/", function (req, res, next) {
 });
 
 
-router.get("/api/city", function (req, res, next) {
-  const validQueries = ["name", "countryCode", "district", "limit"];
+router.get("/api/city", async (req, res, next) => {
+  const validQueries = ["name", "countryCode", "district", "page"];
   const allQueries = req.query;
   const invalidQueries = Object.keys(allQueries).filter(query => !validQueries.includes(query));
-  console.log(allQueries);
+  const limit = 20;
 
-  const { name, countryCode, district, limit } = allQueries;
-  limit = limit || 20;
+  let { name, countryCode, district, page } = allQueries;
+  page = page || 1;
 
   try {
     if (invalidQueries.length > 0) {
-      let error = new Error(`Invalid query parameters: ${invalidQueries.join(", ")}. Query parameters are not permitted.`);
+      let error = new Error(`Invalid query parameters: ${invalidQueries.join(", ")}.`);
+      error.status = 400;
+      throw error;
+    }
+
+    if (parseInt(page) < 0) {
+      let error = new Error(`Invalid page number: ${page}.`);
       error.status = 400;
       throw error;
     }
 
     if (countryCode || name || district) {
-      req.db
-      .from('city')
-      .select('*')
+      const results = [];
+
+      const countObj = await req.db("city")
       .where((builder) => {
         if (countryCode) {
-          builder.where('CountryCode', '=', countryCode);
+          builder.where('city.CountryCode', '=', countryCode);
         }
 
         if (name) {
-          builder.where('Name', '=', name);
+          builder.where('city.Name', '=', name);
         }
 
         if (district) {
-          builder.where("District", "=", district)
+          builder.where("city.District", "=", district)
         }
       })
-      .orderBy('Name')
-      .offset(0)
-      .limit(limit)
-      .then((rows) => {
-        res.json({ Error: false, Message: 'Success', City: rows });
+      .count("name as count");
+
+      const totalCount = countObj.map(el => el["count"]).reduce((acc, curr) => acc + curr, 0);
+
+      const joinedTable = await req.db("city")
+      .join("country", "city.CountryCode", "=", "country.Code")
+      .select("city.Name", "city.CountryCode", "city.District", "country.Name as Country", "country.Continent", "city.Population as CityPopulation")
+      .where((builder) => {
+        if (countryCode) {
+          builder.where('city.CountryCode', '=', countryCode);
+        }
+
+        if (name) {
+          builder.where('city.Name', '=', name);
+        }
+
+        if (district) {
+          builder.where("city.District", "=", district)
+        }
       })
-      .catch((err) => res.json({ Error: true, Message: 'Error in MySQL query' }));
+      .offset((parseInt(page) - 1) * limit)
+      .limit(limit)
+      
+      joinedTable.forEach(row => {
+        const obj = { 
+          Name: row.Name, 
+          Country: row.Country, 
+          District: row.District, 
+          Continent: row.Continent, 
+          Population: row.CityPopulation 
+        }
+        results.push(obj);
+      })
+
+      const pagination = {
+        totalResults: totalCount,  
+        perPage: limit > totalCount ? totalCount: limit, 
+        currentPage: page || 1, 
+        prev: parseInt(page) <= 1 ? null : page - 1,
+        next: parseInt(page) >= Math.ceil(totalCount / limit) ? null : parseInt(page) + 1
+      }
+
+      res.json({ 
+        Error: false, 
+        Message: "Success", 
+        results: results, 
+        pagination: pagination
+      });
     } else {
       req.db
       .from("city")
       .select("name", "district")
       .limit(limit)
       .then((rows) => {
-        res.json({ Error: false, Message: "Success", City: rows });
+        res.json({ Error: false, Message: "Success", results: rows});
       })
       .catch((err) => {
         console.log(err);
